02b9eb7b431f137a41e9bb8c97295b79e7906360
Andrew Ketsko <monicman82@gmail.com>
2023-02-08 13:06:50 +0200

    i_promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+formEl = document.querySelector('form');
+
+formEl.addEventListener('submit', submitFn);
+
+
+function submitFn(e) {
+  e.preventDefault();
+  const { elements: {delay, step, amount} } = e.currentTarget;
+  
+  console.log(delay.value);
+  console.log(step.value);
+  console.log(amount.value);
+
+  createPromise(amount.value, delay.value)
+    .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
+  })
+    .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
+    })
+    .finally(() => {
+      
+    })
+    ;
+};
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+     setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+    resolve({ position, delay });
   } else {
-    // Reject
+    reject({ position, delay });
   }
-}
+  }, delay);
+   });
+ };
+
