572a1f7b18123f6c3b9aab57587b917a78932521
Andrew Ketsko <monicman82@gmail.com>
2023-02-08 14:11:58 +0200

    sum

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,32 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-
 formEl = document.querySelector('form');
+buttonEl = document.querySelector('button');
 
 formEl.addEventListener('submit', submitFn);
 
-
 function submitFn(e) {
   e.preventDefault();
+  buttonEl.classList.add('disable')
   const { elements: {delay, step, amount} } = e.currentTarget;
   
-  console.log(delay.value);
-  console.log(step.value);
-  console.log(amount.value);
+  // console.log(+delay.value);
+  // console.log(+step.value);
+  // console.log(+amount.value);
 
-  createPromise(amount.value, delay.value)
-    .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
-  })
-    .catch(({ position, delay }) => {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
-    })
-    .finally(() => {
-      
-    })
-    ;
+  let fullDelay = +delay.value;
+  
+  for (i = 0; i < amount.value; i += 1) {
+
+    fullDelay = +delay.value + step.value * i; 
+    
+    createPromise(i+1, fullDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
+      });
+  }
 };
 
 
